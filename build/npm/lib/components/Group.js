afd90706b0b436e53250d98b0e4cfe53700c1265
Pavel Linkesch <pavel.linkesch@gmail.com>
2018-10-04 17:48:43 +0200

    change selected story label

--- build/npm/lib/components/Group.js
+++ build/npm/lib/components/Group.js
@@ -1,17 +1,17 @@
-'use strict';
+"use strict";
 
 exports.__esModule = true;
 exports.groupActionsPositionList = undefined;
 
-var _icon = require('antd/lib/icon');
+var _icon = require("antd/lib/icon");
 
 var _icon2 = _interopRequireDefault(_icon);
 
-var _radio = require('antd/lib/radio');
+var _radio = require("antd/lib/radio");
 
 var _radio2 = _interopRequireDefault(_radio);
 
-var _button = require('antd/lib/button');
+var _button = require("antd/lib/button");
 
 var _button2 = _interopRequireDefault(_button);
 
@@ -19,47 +19,47 @@ var _createClass = function () { function defineProperties(target, props) { for 
 
 var _class, _class2, _temp, _initialiseProps;
 
-require('antd/lib/icon/style/css');
+require("antd/lib/icon/style/css");
 
-require('antd/lib/radio/style/css');
+require("antd/lib/radio/style/css");
 
-require('antd/lib/button/style/css');
+require("antd/lib/button/style/css");
 
-var _react = require('react');
+var _react = require("react");
 
 var _react2 = _interopRequireDefault(_react);
 
-var _propTypes = require('prop-types');
+var _propTypes = require("prop-types");
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
-var _reactAddonsShallowCompare = require('react-addons-shallow-compare');
+var _reactAddonsShallowCompare = require("react-addons-shallow-compare");
 
 var _reactAddonsShallowCompare2 = _interopRequireDefault(_reactAddonsShallowCompare);
 
-var _map = require('lodash/map');
+var _map = require("lodash/map");
 
 var _map2 = _interopRequireDefault(_map);
 
-var _startsWith = require('lodash/startsWith');
+var _startsWith = require("lodash/startsWith");
 
 var _startsWith2 = _interopRequireDefault(_startsWith);
 
-var _GroupContainer = require('./containers/GroupContainer');
+var _GroupContainer = require("./containers/GroupContainer");
 
 var _GroupContainer2 = _interopRequireDefault(_GroupContainer);
 
-var _immutable = require('immutable');
+var _immutable = require("immutable");
 
 var _immutable2 = _interopRequireDefault(_immutable);
 
-var _reactAddonsPureRenderMixin = require('react-addons-pure-render-mixin');
+var _reactAddonsPureRenderMixin = require("react-addons-pure-render-mixin");
 
 var _reactAddonsPureRenderMixin2 = _interopRequireDefault(_reactAddonsPureRenderMixin);
 
-var _reactRedux = require('react-redux');
+var _reactRedux = require("react-redux");
 
-var _Item = require('./Item');
+var _Item = require("./Item");
 
 var _Item2 = _interopRequireDefault(_Item);
 
@@ -74,17 +74,17 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 var ButtonGroup = _button2.default.Group;
 var RadioButton = _radio2.default.Button;
 var RadioGroup = _radio2.default.Group;
-var classNames = require('classnames');
+var classNames = require("classnames");
 var groupActionsPositionList = exports.groupActionsPositionList = {
-  topLeft: 'group--actions--tl',
-  topCenter: 'group--actions--tc',
-  topRight: 'group--actions--tr',
-  bottomLeft: 'group--actions--bl',
-  bottomCenter: 'group--actions--bc',
-  bottomRight: 'group--actions--br'
+  topLeft: "group--actions--tl",
+  topCenter: "group--actions--tc",
+  topRight: "group--actions--tr",
+  bottomLeft: "group--actions--bl",
+  bottomCenter: "group--actions--bc",
+  bottomRight: "group--actions--br"
 };
 
-var defaultPosition = 'topRight';
+var defaultPosition = "topRight";
 
 var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (_Component) {
   _inherits(Group, _Component);
@@ -101,24 +101,24 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
   }
 
   _createClass(Group, [{
-    key: 'getRenderType',
+    key: "getRenderType",
     value: function getRenderType(props) {
       var renderType = void 0;
       if (props.dragging && props.dragging.id == props.id) {
-        renderType = props.isForDrag ? 'dragging' : 'placeholder';
+        renderType = props.isForDrag ? "dragging" : "placeholder";
       } else {
-        renderType = props.isForDrag ? null : 'normal';
+        renderType = props.isForDrag ? null : "normal";
       }
       return renderType;
     }
   }, {
-    key: 'render',
+    key: "render",
     value: function render() {
       var renderType = this.getRenderType(this.props);
       if (!renderType) return null;
 
       var styles = {};
-      if (renderType == 'dragging') {
+      if (renderType == "dragging") {
         styles = {
           top: this.props.dragging.y,
           left: this.props.dragging.x,
@@ -127,27 +127,29 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
       }
 
       return _react2.default.createElement(
-        'div',
+        "div",
         {
-          className: classNames("group", "group-or-rule", renderType == 'placeholder' ? 'qb-placeholder' : null, renderType == 'dragging' ? 'qb-draggable' : null),
+          className: classNames("group", "group-or-rule", renderType == "placeholder" ? "qb-placeholder" : null, renderType == "dragging" ? "qb-draggable" : null),
           style: styles,
-          ref: 'group',
-          'data-id': this.props.id
+          ref: "group",
+          "data-id": this.props.id
         },
         _react2.default.createElement(
-          'div',
-          { className: 'group--header' },
+          "div",
+          { className: "group--header" },
           this.renderHeader(),
           this.isGroupTopPosition() && this.renderGroup(this.getGroupPositionClass())
         ),
         this.props.children1 ? _react2.default.createElement(
-          'div',
-          { className: classNames("group--children", this.props.children1.size < 2 && this.props.config.settings.hideConjForOne ? 'hide--line' : '') },
+          "div",
+          {
+            className: classNames("group--children", this.props.children1.size < 2 && this.props.config.settings.hideConjForOne ? "hide--line" : "")
+          },
           this.renderChildren()
         ) : null,
         !this.isGroupTopPosition() && _react2.default.createElement(
-          'div',
-          { className: 'group--footer' },
+          "div",
+          { className: "group--footer" },
           this.renderGroup(this.getGroupPositionClass())
         )
       );
@@ -189,7 +191,7 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
   this._getSetConjunctionHandler = function () {
     var itemKey = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : null;
 
-    var k = '' + itemKey;
+    var k = "" + itemKey;
     var h = _this2._setConjunctionHandlers[k];
     if (!h) {
       h = _this2._setConjunction.bind(_this2, itemKey);
@@ -217,37 +219,33 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
   };
 
   this.isGroupTopPosition = function () {
-    return (0, _startsWith2.default)(_this2.props.config.settings.groupActionsPosition || defaultPosition, 'top');
+    return (0, _startsWith2.default)(_this2.props.config.settings.groupActionsPosition || defaultPosition, "top");
   };
 
   this.renderGroup = function (position) {
     return _react2.default.createElement(
-      'div',
-      { className: 'group--actions ' + position },
+      "div",
+      { className: "group--actions " + position },
       _react2.default.createElement(
         ButtonGroup,
-        {
-          size: _this2.props.config.settings.renderSize || "small"
-        },
+        { size: _this2.props.config.settings.renderSize || "small" },
         !_this2.props.config.settings.readonlyMode && _react2.default.createElement(
           _button2.default,
           {
             icon: _this2.props.storyId != null ? "edit" : "plus",
-            className: 'action action--SELECT-STORY',
+            className: "action action--SELECT-STORY",
             onClick: function onClick(e) {
               e.preventDefault();
               _this2.props.config.storyPicker(_this2.props.setStoryId);
             }
           },
-          _this2.props.storyId != null ? "Select Different Story" : "Select Story",
-          ' ',
-          _this2.props.storyId
+          _this2.props.storyId != null ? "Story: " + _this2.props.storyId : "Select Story"
         ),
         !_this2.props.config.settings.readonlyMode && _react2.default.createElement(
           _button2.default,
           {
-            icon: 'plus',
-            className: 'action action--ADD-RULE',
+            icon: "plus",
+            className: "action action--ADD-RULE",
             onClick: _this2.props.addRule
           },
           _this2.props.config.settings.addRuleLabel || "Add rule"
@@ -255,8 +253,8 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
         !_this2.props.config.settings.readonlyMode && _this2.props.allowFurtherNesting ? _react2.default.createElement(
           _button2.default,
           {
-            className: 'action action--ADD-GROUP',
-            icon: 'plus-circle-o',
+            className: "action action--ADD-GROUP",
+            icon: "plus-circle-o",
             onClick: _this2.props.addGroup
           },
           _this2.props.config.settings.addGroupLabel || "Add group"
@@ -264,9 +262,9 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
         !_this2.props.config.settings.readonlyMode && !_this2.props.isRoot ? _react2.default.createElement(
           _button2.default,
           {
-            type: 'danger',
-            icon: 'delete',
-            className: 'action action--ADD-DELETE',
+            type: "danger",
+            icon: "delete",
+            className: "action action--ADD-DELETE",
             onClick: _this2.props.removeSelf
           },
           _this2.props.config.settings.delGroupLabel !== undefined ? _this2.props.config.settings.delGroupLabel : "Delete"
@@ -279,16 +277,16 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
     var props = _this2.props;
     return props.children1 ? props.children1.map(function (item) {
       return _react2.default.createElement(_Item2.default, {
-        key: item.get('id'),
-        id: item.get('id'),
-        storyId: item.get('storyId')
+        key: item.get("id"),
+        id: item.get("id"),
+        storyId: item.get("storyId")
         //path={props.path.push(item.get('id'))}
-        , path: item.get('path'),
-        type: item.get('type'),
-        properties: item.get('properties'),
+        , path: item.get("path"),
+        type: item.get("type"),
+        properties: item.get("properties"),
         config: props.config,
         actions: props.actions,
-        children1: item.get('children1')
+        children1: item.get("children1")
         //tree={props.tree}
         , treeNodesCnt: props.treeNodesCnt,
         onDragStart: props.onDragStart
@@ -299,10 +297,12 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
   this.renderHeader = function () {
     var renderConjsAsRadios = false;
     return _react2.default.createElement(
-      'div',
-      { className: classNames("group--conjunctions"
+      "div",
+      {
+        className: classNames("group--conjunctions"
         // this.props.children1.size < 2 && this.props.config.settings.hideConjForOne ? 'hide--conj' : ''
-        ) },
+        )
+      },
       _this2.props.config.settings.renderConjsAsRadios ? _react2.default.createElement(
         RadioGroup,
         {
@@ -352,11 +352,14 @@ var Group = (0, _GroupContainer2.default)(_class = (_temp = _class2 = function (
         })
       ),
       _this2.props.config.settings.canReorder && _this2.props.treeNodesCnt > 2 && !_this2.props.isRoot && _react2.default.createElement(
-        'span',
-        { className: "qb-drag-handler", onMouseDown: _this2.handleDraggerMouseDown },
-        ' ',
-        _react2.default.createElement(_icon2.default, { type: 'bars' }),
-        ' '
+        "span",
+        {
+          className: "qb-drag-handler",
+          onMouseDown: _this2.handleDraggerMouseDown
+        },
+        " ",
+        _react2.default.createElement(_icon2.default, { type: "bars" }),
+        " "
       )
     );
   };
