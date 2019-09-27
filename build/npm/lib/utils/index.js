9c15c1d449ec1749ab1e1e5410d0223c7482d2c1
Pavel Linkesch <pavel.linkesch@gmail.com>
2019-09-26 18:59:16 -0700

    expose more utils

--- build/npm/lib/utils/index.js
+++ build/npm/lib/utils/index.js
@@ -1,99 +1,108 @@
-'use strict';
+"use strict";
 
 exports.__esModule = true;
 
-var _queryBuilderFormat = require('./queryBuilderFormat');
+var _queryBuilderFormat = require("./queryBuilderFormat");
 
-Object.defineProperty(exports, 'queryBuilderFormat', {
+Object.defineProperty(exports, "queryBuilderFormat", {
   enumerable: true,
   get: function get() {
     return _queryBuilderFormat.queryBuilderFormat;
   }
 });
 
-var _queryString = require('./queryString');
+var _queryString = require("./queryString");
 
-Object.defineProperty(exports, 'queryString', {
+Object.defineProperty(exports, "queryString", {
   enumerable: true,
   get: function get() {
     return _queryString.queryString;
   }
 });
 
-var _validation = require('./validation');
+var _validation = require("./validation");
 
-Object.defineProperty(exports, 'validateTree', {
+Object.defineProperty(exports, "validateTree", {
   enumerable: true,
   get: function get() {
     return _validation.validateTree;
   }
 });
 
-var _configUtils = require('./configUtils');
+var _configUtils = require("./configUtils");
 
-Object.defineProperty(exports, 'getFieldConfig', {
+Object.defineProperty(exports, "getFieldConfig", {
   enumerable: true,
   get: function get() {
     return _configUtils.getFieldConfig;
   }
 });
-Object.defineProperty(exports, 'getFieldRawConfig', {
+Object.defineProperty(exports, "getFieldRawConfig", {
   enumerable: true,
   get: function get() {
     return _configUtils.getFieldRawConfig;
   }
 });
-Object.defineProperty(exports, 'getFieldPath', {
+Object.defineProperty(exports, "getFieldPath", {
   enumerable: true,
   get: function get() {
     return _configUtils.getFieldPath;
   }
 });
-Object.defineProperty(exports, 'getFieldPathLabels', {
+Object.defineProperty(exports, "getFieldPathLabels", {
   enumerable: true,
   get: function get() {
     return _configUtils.getFieldPathLabels;
   }
 });
-Object.defineProperty(exports, 'getValueLabel', {
+Object.defineProperty(exports, "getValueLabel", {
   enumerable: true,
   get: function get() {
     return _configUtils.getValueLabel;
   }
 });
-Object.defineProperty(exports, 'extendConfig', {
+Object.defineProperty(exports, "extendConfig", {
   enumerable: true,
   get: function get() {
     return _configUtils.extendConfig;
   }
 });
-Object.defineProperty(exports, 'getFieldWidgetConfig', {
+Object.defineProperty(exports, "getFieldWidgetConfig", {
   enumerable: true,
   get: function get() {
     return _configUtils.getFieldWidgetConfig;
   }
 });
-Object.defineProperty(exports, 'getOperatorConfig', {
+Object.defineProperty(exports, "getOperatorConfig", {
   enumerable: true,
   get: function get() {
     return _configUtils.getOperatorConfig;
   }
 });
-Object.defineProperty(exports, 'getWidgetsForFieldOp', {
+Object.defineProperty(exports, "getWidgetsForFieldOp", {
   enumerable: true,
   get: function get() {
     return _configUtils.getWidgetsForFieldOp;
   }
 });
-Object.defineProperty(exports, 'getWidgetForFieldOp', {
+Object.defineProperty(exports, "getWidgetForFieldOp", {
   enumerable: true,
   get: function get() {
     return _configUtils.getWidgetForFieldOp;
   }
 });
-Object.defineProperty(exports, 'getValueSourcesForFieldOp', {
+Object.defineProperty(exports, "getValueSourcesForFieldOp", {
   enumerable: true,
   get: function get() {
     return _configUtils.getValueSourcesForFieldOp;
+  }
+});
+
+var _stuff = require("./stuff");
+
+Object.defineProperty(exports, "calcTextWidth", {
+  enumerable: true,
+  get: function get() {
+    return _stuff.calcTextWidth;
   }
 });
